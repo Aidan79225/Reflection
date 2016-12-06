9dda307fb361eb61138bf879f0132ae2789239f4
Aidan79225 <aidan79225@gmail.com>
2016-12-06 17:11:45 +0800

    Update The maximum-subarray problem.cpp

--- AlgorithmPPT/Ch4/The maximum-subarray problem.cpp
+++ AlgorithmPPT/Ch4/The maximum-subarray problem.cpp
@@ -1,18 +1,35 @@
 int DAYS = 17;
 int price[DAYS] = {100,113,110,85,105,102,86,63,81,101,94,106,101,79,94,90};
+int change[DAYS] = {0};
 int max =0;
 int buyDay=0;
 int sellDay=0;
-void brute-force-solution(){
+class Point{
+    int max = 0;
+    int buyDay = 0;
+    int sellDay = 0;
+}
+Point brute-force-solution(){
+    Point ans;
     for(int i = 0 ; i < DAYS-1 ; i++){
         for(int j =i+1 ; j < DAYS ; j++){
             int temp = price[j]-price[i];
-            if(max < temp){
-                max = temp;
-                buyDay = i;
-                sellDay = i;
+            if(ans.max < temp){
+                ans.max = temp;
+                ans.buyDay = i;
+                ans.sellDay = i;
             }
         }
     }
+    return ans;
+}
+void init(){
+    for(int i=1;i<DAYS;i++){
+        change[i] = price[i]-price[i-1];
+    }
+}
+void divide(){
+    init();
+    
 }
 
