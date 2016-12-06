bb3b16ae854a0b4574164320be003a787a66aebf
Aidan79225 <aidan79225@gmail.com>
2016-12-06 16:27:41 +0800

    Create The maximum-subarray problem(brute-force solution).cpp

---
+++ AlgorithmPPT/Ch4/The maximum-subarray problem(brute-force solution).cpp
@@ -0,0 +1,16 @@
+int DAYS = 17;
+int price[DAYS] = {100,113,110,85,105,102,86,63,81,101,94,106,101,79,94,90};
+int max =0;
+int buyDay=0;
+int sellDay=0;
+
+for(int i = 0 ; i < DAYS-1 ; i++){
+    for(int j =i+1 ; j < DAYS ; j++){
+        int temp = price[j]-price[i];
+        if(max < temp){
+            max = temp;
+            buyDay = i;
+            sellDay = i;
+        }
+    }
+}
